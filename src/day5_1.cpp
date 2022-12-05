1cfd78ec1fcc673237c65129ef7f05972e303872
Alexei Zakharov <alexeiz@gmail.com>
2022-12-05 01:34:33 -0500

    solve puzzle 1 day 5

---
+++ src/day5_1.cpp
@@ -0,0 +1,26 @@
+#include "common.hpp"
+#include "puzzle_reg.hpp"
+#include "day5.hpp"
+
+using namespace std;
+namespace views = ranges::views;
+
+namespace {
+/// Which crate will end up on top of each stack after rearrangement.
+/// https://adventofcode.com/2022/day/5
+puzzle_reg _{"5.1", []{
+    for (auto [num, from, to]: day5::move_instr)
+    {
+        for ([[maybe_unused]] auto _: views::iota(0, num))
+        {
+            day5::stacks[to - 1].push_back(day5::stacks[from - 1].back());
+            day5::stacks[from - 1].pop_back();
+        }
+    }
+
+    for (auto const &st: day5::stacks)
+        fmt::print("{}", st.back());
+
+    fmt::print("\n");
+}};
+}
