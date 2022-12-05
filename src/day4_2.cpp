d2b3a01252220b965a29eb5685ed92a01de474bb
Alexei Zakharov <alexeiz@gmail.com>
2022-12-05 00:55:45 -0500

    solve puzzle 2 day 4

--- src/day4_2.cpp
+++ src/day4_2.cpp
@@ -6,18 +6,18 @@ using namespace std;
 namespace views = ranges::views;
 
 namespace {
-/// Count the number of fully overlapping section ranges.
-/// https://adventofcode.com/2022/day/4
-puzzle_reg _{"4.1", []{
+/// Count the number of overlapping section ranges.
+/// https://adventofcode.com/2022/day/4#part2
+puzzle_reg _{"4.2", []{
     int overlapping = 0;
     for (auto const & [rng1, rng2]: day4::sections)
     {
         auto const & [r1x, r1y] = rng1;
         auto const & [r2x, r2y] = rng2;
-        if ((r1x <= r2x and r2y <= r1y) or (r2x <= r1x and r1y <= r2y))
+        if ((r1x <= r2x and r2x <= r1y) or (r2x <= r1x and r1x <= r2y))
             ++overlapping;
     }
 
-    fmt::print("total fully overlapping section ranges: {}\n", overlapping);
+    fmt::print("total overlapping section ranges: {}\n", overlapping);
 }};
 }
